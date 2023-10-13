899878084b05053538c051fc23d32af031c219e0
artick788 <60552965+artick788@users.noreply.github.com>
2023-10-13 17:23:20 +0200

    project setup

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "SyriusShadingLanguage/include/SrslCompiler.hpp"
+
+void compile(const std::string& vertex, const std::string& fragment, const std::string& output){
+    auto vsm = Srsl::createShaderModule(vertex);
+    vsm->exportAstDot("VSM.dot");
+    vsm->exportSymbolTableHtml("VSM.html");
+
+    auto fsm = Srsl::createShaderModule(fragment);
+    fsm->exportAstDot("FSM.dot");
+    fsm->exportSymbolTableHtml("FSM.html");
+
+    auto program = Srsl::createShaderProgram();
+    program->addShaderModule(vsm);
+    program->addShaderModule(fsm);
+    program->link();
+
+    Srsl::CppDescriptor desc;
+    desc.outputFile = output;
+    program->exportCpp(desc);
+}
+
+int main(int argc, char** argv) {
+    try {
+        compile("SRSLShaders/Basic-vs.srsl", "SRSLShaders/Basic-fs.srsl", "runtime/Basic");
+
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+}
